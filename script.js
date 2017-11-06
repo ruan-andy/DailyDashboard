6aaaacc7ed2f8f134fea3634084fe5e1752a722d
saivuppala <svuppala@ucsd.edu>
2017-11-05 19:00:39 -0800

    bills changed

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-$(document).ready(function(){
-	$('about').onMouseOver
-
+$(document).ready(function() {
+  $("#addBillsButton").click(addBills);
 })
+function addBills() {
+  console.log("Testing");
+  //$("#item1").toggle();
+  var list = $("#bList");
+  //var
+  var item = $('#boxedBills');
+  list.append("<p>Testing</p>");
+
+  
+}
