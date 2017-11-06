429b04b7efcc74bdd7d859f47339fdb72476077e
Prasanth Abraham <santh96@yahoo.com>
2017-11-05 18:37:28 -0800

    Adds Testing txt when btn clicked on Medicine page

---
+++ medscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#addMedsButton").click(addMeds);
+})
+function addMeds() {
+  console.log("Testing");
+  //$("#item1").toggle();
+  var list = $("#mList");
+  //var
+  list.append("<p>Testing</p>")
+}
