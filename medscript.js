e18f5407d32246682a01bbd391505520a302798b
Prasanth Abraham <santh96@yahoo.com>
2017-11-06 17:26:40 -0800

    added popup to Add New Medication

--- medscript.js
+++ medscript.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  $("#addMedsButton").click(addMeds);
+  $("#addMedsButton").click(openPopup);
+  $("#cancelAddMed").click(closePopup);
+  $("#addNewMed").click(addMeds);
 
 })
 var simpleData = {'title': 'Eye drops', 'notes': '2 drops per eye', 'time': '3:00pm', 'index': 1};
@@ -13,11 +15,43 @@ var medData = [
 {'title': 'Herbal tea', 'notes': '1 cup at night', 'time': '9:00pm', 'date': '11/9/17', 'index': 6}
 ]
 
+function openPopup(){
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  // When the user clicks the button, open the modal
+  modal.style.display = "block";
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+}
+
+function closePopup(){
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  modal.style.display = "none";
+}
+
 function addMeds() {
+
 
   console.log("Testing");
 
   if(dataIndex<3){
+    //var person = prompt("Bill name:", "Electric bill");
     var source = $("#today-template").html();
     var template = Handlebars.compile(source);
 
@@ -40,5 +74,7 @@ function addMeds() {
 
 
   dataIndex++;
+
+  closePopup();
 
 }
